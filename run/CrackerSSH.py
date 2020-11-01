8e6cb9850a145ca1a0f3cc95a212a41716edac62
Joram Stith <43012984+Jstith@users.noreply.github.com>
2020-11-01 13:41:55 -0500

    fixed spelling

--- run/CrackerSSH.py
+++ run/CrackerSSH.py
@@ -16,7 +16,7 @@ class CrackerSSH():
         else:
             self.isSSH = False
 
-    # If a identify option is available later, we'd send an override bool here to get evaluated
+    # If an identify option is available later, we'd send an override bool here to get evaluated
     def userCheck(self):
         if(len(self.__args) >= 4):
             for x in range(len(self.__args)):
